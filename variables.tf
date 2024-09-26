cdfe4d00d1da3bb2a8c9516a500cd70e08485e84
bh-siva-git <siva@bighammer.ai>
2024-09-26 08:09:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "zones" {
   }))
   description = "A map defining the zones with comments and optional tags."
 }
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
